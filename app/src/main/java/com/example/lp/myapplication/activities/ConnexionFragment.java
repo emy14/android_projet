0a20bd5c16e8327612d2ea75f684af6b6e1a9f21
Noemie <noemiemais@gmail.com>
2018-02-27 17:14:28 +0100

    fb init

--- app/src/main/java/com/example/lp/myapplication/activities/ConnexionFragment.java
+++ app/src/main/java/com/example/lp/myapplication/activities/ConnexionFragment.java
@@ -1,10 +1,18 @@
 package com.example.lp.myapplication.activities;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 import com.example.lp.myapplication.R;
 
@@ -12,12 +20,49 @@ import com.example.lp.myapplication.R;
  * Created by lp on 24/01/2018.
  */
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
 public class ConnexionFragment extends Fragment {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+         callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
         return inflater.inflate(R.layout.connexion_activity, container, false);
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
